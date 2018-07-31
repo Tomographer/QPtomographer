662257d11a31e0e6e48e328ffad140b64a808217
Philippe Faist <phfaist@gmail.com>
2018-08-01 01:34:31 +0200

    ...

--- doc/conf.py
+++ doc/conf.py
@@ -21,6 +21,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import QPtomographer
+
 
 # -- General configuration ------------------------------------------------
 
@@ -31,9 +33,11 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.mathjax']
+    'sphinx.ext.mathjax'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
